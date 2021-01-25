a9b6bf15bc2c284c27c31ae0fbb1143625ae760e
alexander <alexander-1313>
2021-01-25 03:34:11 +0300

    add comment controller

---
+++ src/main/java/ru/leverx/blog/controller/CommentController.java
@@ -0,0 +1,11 @@
+package ru.leverx.blog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users/articles/{id}/comments")
+public class CommentController {
+
+
+}
