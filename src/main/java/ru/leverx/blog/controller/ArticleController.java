56cbd93c89abc9c29e5befdc3d3434bec8141bfa
alexander <alexander-1313>
2021-01-25 16:54:29 +0300

    update Article controller v2.0

--- src/main/java/ru/leverx/blog/controller/ArticleController.java
+++ src/main/java/ru/leverx/blog/controller/ArticleController.java
@@ -4,43 +4,74 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.leverx.blog.entity.Article;
 import ru.leverx.blog.entity.Status;
+import ru.leverx.blog.entity.User;
 import ru.leverx.blog.service.ArticleService;
+import ru.leverx.blog.service.UserService;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/users/{userId}/articles")
 public class ArticleController {
 
-    @Autowired
     private ArticleService articleService;
+    private UserService userService;
 
-    @PostMapping("/articles")
-    public Article addPost(@RequestParam("title") String title,
+    @Autowired
+    public ArticleController(ArticleService articleService, UserService userService) {
+        this.articleService = articleService;
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public Article addPost(@PathVariable String userId,
+                           @RequestParam("title") String title,
                            @RequestParam("text") String text,
                            @RequestParam("status") String status){
 
-        Article article = new Article(title, text, Status.valueOf(status), new Date(), new Date());
+        User user = userService.getById(Integer.parseInt(userId));
+
+        Article article = new Article(title, text, Status.valueOf(status), new Date(), new Date(), user);
         articleService.save(article);
 
         return article;
     }
 
-    @GetMapping("/articles")
-    public List<Article> getAllPublicPosts(){
+    @GetMapping
+    public List<Article> getAllPublicPosts(@PathVariable String userId,
+                                           @RequestParam Map<String, String> allRequestParam){
 
-        return articleService.findByStatus(Status.PUBLIC);
+//        List<Article> articles = articleService.findByStatus(Status.PUBLIC);
+
+        return articleService.filterArticles(allRequestParam);
     }
 
-    @DeleteMapping("/articles/{id}")
-    public void deleteArticleById(@PathVariable String id){
+    @DeleteMapping("/{id}")
+    public void deleteArticleById(@PathVariable String userId,
+                                  @PathVariable String id){
         articleService.deleteById(Integer.parseInt(id));
     }
 
+    @PutMapping("/{id}")
+    public void editArticleById(@PathVariable String userId,
+                                @PathVariable String id,
+                                @RequestParam("title") String title,
+                                @RequestParam("text") String text,
+                                @RequestParam("status") String status){
+
+        articleService.updateById(Integer.parseInt(id), title, text, status);
+    }
+
+    @GetMapping("/my")
+    public List<Article> getArticleByUser(@PathVariable String userId){
+        User user = userService.getById(Integer.parseInt(userId));
+
+        return articleService.findByUser(user);
+    }
+
     /**
-     *добавить редактирование статьи
-     * получить посты авторизованного пользователя
      * фильтрация
      */
 }
