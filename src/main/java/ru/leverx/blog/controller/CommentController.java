1acded9a377d26d008f7cb4802eda3129f8c1877
alexander <alexander-1313>
2021-01-27 05:37:01 +0300

    update comment controller v3.0

--- src/main/java/ru/leverx/blog/controller/CommentController.java
+++ src/main/java/ru/leverx/blog/controller/CommentController.java
@@ -1,5 +1,6 @@
 package ru.leverx.blog.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +10,10 @@ import ru.leverx.blog.entity.User;
 import ru.leverx.blog.service.ArticleService;
 import ru.leverx.blog.service.CommentService;
 import ru.leverx.blog.service.UserService;
+import ru.leverx.blog.util.RequestUtil;
+import ru.leverx.blog.util.View;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -21,51 +25,63 @@ public class CommentController {
     private final CommentService commentService;
     private final UserService userService;
     private final ArticleService articleService;
+    private final RequestUtil requestUtil;
 
     @Autowired
-    public CommentController(CommentService commentService, UserService userService, ArticleService articleService) {
+    public CommentController(CommentService commentService, UserService userService, ArticleService articleService, RequestUtil requestUtil) {
         this.commentService = commentService;
         this.userService = userService;
         this.articleService = articleService;
+        this.requestUtil = requestUtil;
     }
 
     @PostMapping
+    @JsonView(View.UI.class)
     public void addComment(@PathVariable String userId,
                            @PathVariable String articleId,
-                           @RequestParam("text") String text){
-
-        User user = userService.getById(Integer.parseInt(userId));
-        Article article = articleService.findById(Integer.parseInt(articleId));
-        Comment comment = new Comment(text, new Date(), user, article);
-
-        commentService.save(comment);
+                           @RequestParam("text") String text,
+                           HttpServletRequest request){
+        if(requestUtil.isConfirmUser(request, userId)) {
+            User user = userService.getById(Integer.parseInt(userId));
+            Article article = articleService.findById(Integer.parseInt(articleId));
+            Comment comment = new Comment(text, new Date(), user, article);
+            commentService.save(comment);
+        }
     }
 
     @GetMapping
+    @JsonView(View.UI.class)
     public List<Comment> findCommentByArticle(@PathVariable String userId,
                                               @PathVariable String articleId,
-                                              @RequestParam Map<String, String> allRequestParam){
+                                              @RequestParam Map<String, String> allRequestParam){ //TODO
 
         return commentService.filterComments(allRequestParam, articleId);
     }
 
     @GetMapping("/{commentId}")
+    @JsonView(View.UI.class)
     public Comment getCommentById(@PathVariable String userId,
                                   @PathVariable String articleId,
                                   @PathVariable String commentId){
+
+        User user = userService.getById(Integer.parseInt(userId));
+        Article article = articleService.findById(Integer.parseInt(articleId));
 
         return commentService.findById(Integer.parseInt(commentId));
     }
 
     @DeleteMapping("/{commentId}")
+    @JsonView(View.UI.class)
     public void deleteCommentById(@PathVariable String userId,
                                   @PathVariable String articleId,
-                                  @PathVariable String commentId){
+                                  @PathVariable String commentId,
+                                  HttpServletRequest request){
 
-        commentService.deleteById(Integer.parseInt(commentId));
+        if(requestUtil.isConfirmUser(request, userId)) {
+            Article article = articleService.findById(Integer.parseInt(articleId));
+            User user = userService.getById(Integer.parseInt(userId));
+            commentService.deleteCommentByIdAndArticleAndCommentUser(Integer.parseInt(commentId), article, user);
+        }
     }
 
-    /**
-     * настроить отображение json
-     */
 }
