cacac581b48042a5dad27579230781077c11e0c2
alexander <alexander-1313>
2021-01-28 23:30:20 +0300

    add article service

--- src/main/java/ru/leverx/blog/service/ArticleService.java
+++ src/main/java/ru/leverx/blog/service/ArticleService.java
@@ -1,35 +1,20 @@
 package ru.leverx.blog.service;
 
 import ru.leverx.blog.entity.Article;
-import ru.leverx.blog.entity.Comment;
 import ru.leverx.blog.entity.Status;
 import ru.leverx.blog.entity.User;
 
 import java.util.List;
-import java.util.Map;
 
 public interface ArticleService {
 
+    void deleteById(Integer userId, Integer articleId);
+
     void save(Article article);
 
-    void delete(Article article);
-
-    List<Article> findAll();
-
-    Article findById(Integer id);
-
-    List<Article> findByStatus(Status status);
+    void updateById(Integer articleId, Integer userId, String title, String text, String status);
 
     List<Article> findByUser(User user);
 
-    void deleteById(Integer id);
-
-    void updateById(Integer id, Integer userId, String title, String text, String status);
-
-    List<Article> filterArticles(Map<String, String> allRequestParams);
-
-    void deleteArticlesByIdAndUser(Integer id, User user);
-
-    Article findArticleByIdAndUser(Integer id, User user);
-
+    Article findById(Integer id);
 }
--- src/main/java/ru/leverx/blog/service/impl/ArticleServiceImpl.java
+++ src/main/java/ru/leverx/blog/service/impl/ArticleServiceImpl.java
@@ -2,68 +2,55 @@ package ru.leverx.blog.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.leverx.blog.comparator.ArticleComparator;
 import ru.leverx.blog.entity.Article;
 import ru.leverx.blog.entity.Status;
 import ru.leverx.blog.entity.User;
 import ru.leverx.blog.repository.ArticleRepository;
+import ru.leverx.blog.repository.UserRepository;
 import ru.leverx.blog.service.ArticleService;
+import ru.leverx.blog.service.CommentService;
 import ru.leverx.blog.service.UserService;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class ArticleServiceImpl implements ArticleService {
 
-    private final ArticleRepository repository;
-    private final UserService service;
+    @Autowired
+    private UserRepository userRepository;
+
+    private final ArticleRepository articleRepository;
+    private final UserService userService;
 
     @Autowired
     public ArticleServiceImpl(ArticleRepository repository, UserService service) {
-        this.repository = repository;
-        this.service = service;
+        this.articleRepository = repository;
+        this.userService = service;
+    }
+
+    @Override
+    public void deleteById(Integer userId, Integer articleId) {
+        User user = userService.findById(userId);
+
+        Article article = articleRepository.findByIdAndUser(articleId, user);
+
+        if(article != null) {
+            articleRepository.deleteArticle(articleId);
+        }
     }
 
     @Override
     public void save(Article article) {
-        repository.save(article);
+        if (article != null) {
+            articleRepository.save(article);
+        }
     }
 
     @Override
-    public void delete(Article article) {
-        repository.delete(article);
-    }
-
-    @Override
-    public List<Article> findAll() {
-        return repository.findAll();
-    }
-
-    @Override
-    public Article findById(Integer id) {
-        return repository.getOne(id);
-    }
-
-    @Override
-    public List<Article> findByStatus(Status status) {
-        return repository.findArticleByStatus(status);
-    }
-
-    @Override
-    public List<Article> findByUser(User user) {
-        return repository.findArticleByUser(user);
-    }
-
-    @Override
-    public void deleteById(Integer id) {
-        repository.deleteArticlesById(id);
-    }
-
-    @Override
-    public void updateById(Integer id, Integer userId, String title, String text, String status) {
-        Article article = findArticleByIdAndUser(id, service.getById(userId));
-        if(article != null) {
+    public void updateById(Integer articleId, Integer userId, String title, String text, String status) {
+        Article article = articleRepository.findByIdAndUser(articleId, userService.findById(userId));
+        if (article != null) {
             if (title != null) {
                 article.setTitle(title);
             }
@@ -76,72 +63,17 @@ public class ArticleServiceImpl implements ArticleService {
 
             article.setUpdatedAt(new Date());
 
-            repository.save(article);
+            articleRepository.save(article);
         }
-    }
-
-    private List<Article> findArticleByTitleAndUser(String title, User user) {
-        List<Article> articles =  repository.findArticleByTitleAndUser(title, user);
-        List<Article> resArticles =  articles.stream().filter(a -> a.getStatus() == Status.PUBLIC).collect(Collectors.toList());
-        return  resArticles;
-    }
-
-    private List<Article> sort(List<Article> articles, String sort, String order) {
-        if(sort == null){
-            return articles;
-        }
-        if(order.equalsIgnoreCase("asc") || order == null) {
-            Collections.sort(articles, ArticleComparator.valueOf(sort.toUpperCase(Locale.ROOT)));
-        }else{
-            Collections.sort(articles, ArticleComparator.valueOf(sort.toUpperCase(Locale.ROOT)).reversed());
-        }
-        return articles;
-    }
-
-    private List<Article> filterArticlesWithSkipAndLimit(List<Article> articles, Integer skip, Integer limit) {
-        List<Article> filteredArticles = new ArrayList<Article>();
-        Integer lim = 0;
-
-        if(skip == null){
-            skip = 0;
-        }
-        if(limit == null){
-            limit = articles.size();
-        }
-
-        for(int i = skip; i < articles.size(); i++){
-            if(lim < limit){
-                Article article = articles.get(i);
-                filteredArticles.add(article);
-                lim++;
-            }
-        }
-
-        return filteredArticles;
     }
 
     @Override
-    public List<Article> filterArticles(Map<String, String> allRequestParams) {
-        String title = allRequestParams.get("title");
-        String author = allRequestParams.get("author");
-
-        List<Article> articles = findArticleByTitleAndUser(allRequestParams.get(title), service.getById(Integer.parseInt(allRequestParams.get(author))));
-        sort(articles, allRequestParams.get("sort"), allRequestParams.get("order"));
-        articles = filterArticlesWithSkipAndLimit(articles, Integer.parseInt(allRequestParams.get("skip")), Integer.parseInt(allRequestParams.get("limit")));
-        System.out.println(allRequestParams.size());
-
-        return articles;
+    public List<Article> findByUser(User user) {
+        return articleRepository.findByUser(user);
     }
 
     @Override
-    public void deleteArticlesByIdAndUser(Integer id, User user) {
-        repository.deleteArticlesByIdAndUser(id, user);
+    public Article findById(Integer id) {
+        return articleRepository.getOne(id);
     }
-
-    @Override
-    public Article findArticleByIdAndUser(Integer id, User user) {
-        return repository.findArticleByIdAndUser(id, user);
-    }
-
-
 }
