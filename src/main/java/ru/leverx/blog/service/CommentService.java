6f434e7e4c32f8861ab707c84895d897bf6b1746
alexander <alexander-1313>
2021-01-28 23:30:12 +0300

    add comment service

--- src/main/java/ru/leverx/blog/service/CommentService.java
+++ src/main/java/ru/leverx/blog/service/CommentService.java
@@ -4,24 +4,11 @@ import ru.leverx.blog.entity.Article;
 import ru.leverx.blog.entity.Comment;
 import ru.leverx.blog.entity.User;
 
-import java.util.List;
-import java.util.Map;
-
 public interface CommentService {
 
     void save(Comment comment);
 
-    void deleteById(Integer id);
-
     Comment findById(Integer id);
 
-    List<Comment> findAll();
-
-    List<Comment> findCommentByArticle(Article article);
-
-    List<Comment> filterComments(Map<String, String> allRequestParams, String articleId);
-
     void deleteCommentByIdAndArticleAndCommentUser(Integer id, Article article, User user);
-
-    Comment findCommentByCommentUserAndArticleAndId(User commentUser, Article article, Integer id);
 }
--- src/main/java/ru/leverx/blog/service/impl/CommentServiceImpl.java
+++ src/main/java/ru/leverx/blog/service/impl/CommentServiceImpl.java
@@ -2,107 +2,53 @@ package ru.leverx.blog.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.leverx.blog.comparator.CommentComparator;
 import ru.leverx.blog.entity.Article;
 import ru.leverx.blog.entity.Comment;
 import ru.leverx.blog.entity.User;
+import ru.leverx.blog.repository.ArticleRepository;
 import ru.leverx.blog.repository.CommentRepository;
+import ru.leverx.blog.repository.UserRepository;
 import ru.leverx.blog.service.ArticleService;
 import ru.leverx.blog.service.CommentService;
-
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.stream.Collectors;
+import ru.leverx.blog.service.UserService;
 
 @Service
 public class CommentServiceImpl implements CommentService {
 
-    private final CommentRepository repository;
+    private final CommentRepository commentRepository;
     private final ArticleService articleService;
+    private final UserService userService;
 
     @Autowired
-    public CommentServiceImpl(CommentRepository repository, ArticleService articleService) {
-        this.repository = repository;
+    private UserRepository userRepository;
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    public CommentServiceImpl(CommentRepository repository, ArticleService articleService, UserService userService) {
+        this.commentRepository = repository;
         this.articleService = articleService;
+        this.userService = userService;
     }
 
     @Override
     public void save(Comment comment) {
-        repository.save(comment);
-    }
-
-    @Override
-    public void deleteById(Integer id) {
-        repository.deleteCommentById(id);
+        if(comment != null){
+          commentRepository.save(comment);
+        }
     }
 
     @Override
     public Comment findById(Integer id) {
-        return repository.getOne(id);
-    }
-
-    @Override
-    public List<Comment> findAll() {
-        return repository.findAll();
-    }
-
-    @Override
-    public List<Comment> findCommentByArticle(Article article){
-        return repository.findCommentByArticle(article);
-    }
-
-    @Override
-    public List<Comment> filterComments(Map<String, String> allRequestParams, String articleId) {
-        Article article = articleService.findById(Integer.parseInt(articleId));
-        List<Comment> filteredComments = repository.findCommentByArticle(article);
-
-        if(allRequestParams.get("author") != null){
-            filteredComments = filterByAuthorId(filteredComments, Integer.parseInt(allRequestParams.get("author")));
-        }
-        if(allRequestParams.get("skip") != null){
-            filteredComments = filterBySkip(filteredComments, Integer.parseInt(allRequestParams.get("skip")));
-        }
-        if(allRequestParams.get("limit") != null){
-            filteredComments = filterByLimit(filteredComments, Integer.parseInt(allRequestParams.get("limit")));
-        }
-        if(allRequestParams.get("sort") != null && allRequestParams.get("order").equalsIgnoreCase("desc")){
-            filteredComments = sortDescOrder(filteredComments, allRequestParams.get("sort"));
-        }else if(allRequestParams.get("sort") != null){
-            filteredComments = sortAscOrder(filteredComments, allRequestParams.get("sort"));
-        }
-
-        return filteredComments;
+        return commentRepository.getOne(id);
     }
 
     @Override
     public void deleteCommentByIdAndArticleAndCommentUser(Integer id, Article article, User user) {
-        repository.deleteCommentByIdAndArticleAndCommentUser(id, article, user);
-    }
+        Comment comment = commentRepository.findByIdAndCommentUserAndArticle(id, user, article);
 
-    @Override
-    public Comment findCommentByCommentUserAndArticleAndId(User commentUser, Article article, Integer id) {
-        return repository.findCommentByCommentUserAndArticleAndId(commentUser, article, id);
+        if(comment != null){
+            commentRepository.deleteComment(id);
+        }
     }
-
-    private List<Comment> filterByAuthorId(List<Comment> comments, Integer id){
-        return comments.stream().filter(a -> a.getCommentUser().getId().equals(id)).collect(Collectors.toList());
-    }
-
-    private List<Comment> filterBySkip(List<Comment> comments, Integer skip){
-        return comments.stream().skip(skip).collect(Collectors.toList());
-    }
-
-    private List<Comment> filterByLimit(List<Comment> comments, Integer limit){
-        return comments.stream().limit(limit).collect(Collectors.toList());
-    }
-
-    private List<Comment> sortAscOrder(List<Comment> comments, String sort){
-        return comments.stream().sorted(CommentComparator.valueOf(sort)).collect(Collectors.toList());
-    }
-
-    private List<Comment> sortDescOrder(List<Comment> comments, String sort){
-        return comments.stream().sorted(CommentComparator.valueOf(sort).reversed()).collect(Collectors.toList());
-    }
-
 }
