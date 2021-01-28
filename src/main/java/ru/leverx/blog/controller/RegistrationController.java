561f00240ef6b98edc8afda744f946f3100e903b
alexander <alexander-1313>
2021-01-28 23:29:43 +0300

    add registration controller

--- src/main/java/ru/leverx/blog/controller/RegistrationController.java
+++ src/main/java/ru/leverx/blog/controller/RegistrationController.java
@@ -2,15 +2,11 @@ package ru.leverx.blog.controller;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.MessageSource;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.bind.annotation.*;
 import ru.leverx.blog.configuration.jwt.JwtProvider;
 import ru.leverx.blog.entity.User;
+import ru.leverx.blog.service.MailService;
+import ru.leverx.blog.service.RedisService;
 import ru.leverx.blog.service.UserService;
 import ru.leverx.blog.util.RequestUtil;
 import ru.leverx.blog.util.View;
@@ -25,12 +21,16 @@ public class RegistrationController {
     private UserService service;
     private JwtProvider jwtProvider;
     private RequestUtil requestUtil;
+    private RedisService redisService;
+    private MailService mailService;
 
     @Autowired
-    public RegistrationController(UserService service, JwtProvider jwtProvider, RequestUtil requestUtil) {
+    public RegistrationController(UserService service, JwtProvider jwtProvider, RequestUtil requestUtil, RedisService redisService, MailService mailService) {
         this.service = service;
         this.jwtProvider = jwtProvider;
         this.requestUtil = requestUtil;
+        this.redisService = redisService;
+        this.mailService = mailService;
     }
 
     @JsonView(View.UI.class)
@@ -40,11 +40,23 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public void registerUser(@RequestParam("firstName") String firstName,
+    public String registerUser(@RequestParam("firstName") String firstName,
                              @RequestParam("lastName") String lastName,
                              @RequestParam("email") String email,
                              @RequestParam("password") String password) {
-        service.save(new User(firstName, lastName, password, email, new Date()));
+
+        User dbUser = service.findByEmail(email);
+
+        if(dbUser == null) {
+            User user = new User(firstName, lastName, password, email, new Date());
+            service.save(user);
+            String token = jwtProvider.generateToken(email);
+            redisService.save(token, email);
+            mailService.sendEmail(email, token, "/confirm");
+            return token;
+        }
+
+        return "";
     }
 
     @GetMapping("/auth")
@@ -52,8 +64,13 @@ public class RegistrationController {
                      @RequestParam("password") String password){
 
         User user = service.findByPasswordAndEmail(password, email);
-        String token = jwtProvider.generateToken(user.getEmail());
-        return token;
+
+        if(user != null && user.isEnabled()) {
+            String token = jwtProvider.generateToken(user.getEmail());
+            return token;
+        }
+
+        return "";
     }
 
     @JsonView(View.UI.class)
@@ -62,16 +79,43 @@ public class RegistrationController {
                         HttpServletRequest request){
 
         if(requestUtil.isConfirmUser(request, id)){
-            return service.getById(Integer.parseInt(id));
+            return service.findById(Integer.parseInt(id));
         }
         return null;
     }
 
+
+    @PostMapping("/auth/confirm")
+    public void confirmRegistration(@RequestParam("token") String token){
+
+        String email = redisService.getByToken(token);
+
+        if(email != null){
+            service.registerByEmail(email);
+        }
+    }
+
+    @PostMapping("/auth/forgot_password")
+    public void forgotPassword(@RequestParam("email") String email){
+        if(email != null){
+            mailService.sendEmail(email, jwtProvider.generateToken(email), "/reset");
+        }
+    }
+
+    @PostMapping("/auth/reset")
+    public void resetPassword(@RequestParam("token") String token,
+                              @RequestParam("new_password") String newPassword){
+
+        String email = redisService.getByToken(token);
+
+        if(email != null){
+            service.resetPasswordByEmail(email, newPassword);
+        }
+    }
+
+
+
     /**
-     * auth/forgot_password/{email} POST
-     * auth/reset/{new paswrod}
-     * auth/check_code/{code}
+     * auth/check_code/{code} GET
      */
-
-
 }
