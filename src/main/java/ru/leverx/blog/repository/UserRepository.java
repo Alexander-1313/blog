1832fe70915a7f7833888c7c3300d5f92b39e9d6
alexander <alexander-1313>
2021-01-28 23:33:32 +0300

    update user repository

--- src/main/java/ru/leverx/blog/repository/UserRepository.java
+++ src/main/java/ru/leverx/blog/repository/UserRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.leverx.blog.entity.User;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
     User findByEmail(String email);
-    User findByPasswordAndEmail(String password, String email);
+
+    User findAllByPasswordAndEmail(String password, String email);
 }
