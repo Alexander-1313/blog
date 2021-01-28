e896616ffcc9b8c8a7230a6d7b1469652a6078f7
alexander <alexander-1313>
2021-01-28 23:33:26 +0300

    update comment repository

--- src/main/java/ru/leverx/blog/repository/CommentRepository.java
+++ src/main/java/ru/leverx/blog/repository/CommentRepository.java
@@ -1,6 +1,8 @@
 package ru.leverx.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.leverx.blog.entity.Article;
 import ru.leverx.blog.entity.Comment;
@@ -12,14 +14,10 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
-    @Transactional
-    void deleteCommentById(Integer id);
+    Comment findByIdAndCommentUserAndArticle(Integer id, User user, Article article);
 
     @Transactional
-    void deleteCommentByIdAndArticleAndCommentUser(Integer id, Article article, User user);
-
-    List<Comment> findCommentByArticle(Article article);
-
-    Comment findCommentByCommentUserAndArticleAndId(User commentUser, Article article, Integer id);
-
+    @Modifying
+    @Query("DELETE FROM Comment c WHERE c.id = :id")
+    void deleteComment(Integer id);
 }
