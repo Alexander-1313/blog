f117dce48f06125d2bda94e7bb0b7c2f15e29d22
alexander <alexander-1313>
2021-01-28 23:31:37 +0300

    update redis config

--- src/main/java/ru/leverx/blog/configuration/RedisConfig.java
+++ src/main/java/ru/leverx/blog/configuration/RedisConfig.java
@@ -1,24 +1,32 @@
 package ru.leverx.blog.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import redis.clients.jedis.Jedis;
 
-//@Configuration
-//public class RedisConfig {
-//
-//    @Bean
-//    JedisConnectionFactory jedisConnectionFactory(){
-//        return new JedisConnectionFactory();
-//    }
-//
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate() {
-//        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-//        template.setConnectionFactory(jedisConnectionFactory());
-//        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
-//        return template;
-//    }
-//}
+@Configuration
+@ComponentScan("ru.leverx.blog")
+public class RedisConfig {
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory(){
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        return template;
+    }
+
+    @Bean("jedis")
+    Jedis getJedis(){
+        return new Jedis("localhost");
+    }
+}
