eef2a133fe55e7640fec113ace8a07551efd0404
alexander <alexander-1313>
2021-01-30 05:27:05 +0300

    update Tag entity for beautiful JSON

--- src/main/java/ru/leverx/blog/entity/Tag.java
+++ src/main/java/ru/leverx/blog/entity/Tag.java
@@ -1,6 +1,8 @@
 package ru.leverx.blog.entity;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
+import ru.leverx.blog.util.View;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -14,12 +16,15 @@ public class Tag {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonView(View.UI.class)
     private Integer id;
 
     @Column(name = "name", unique = true)
+    @JsonView(View.UI.class)
     private String name;
 
     @ManyToMany(mappedBy = "tags")
+    @JsonView(View.REST.class)
     private List<Article> articles = new ArrayList<>();
 
     public Tag() {
