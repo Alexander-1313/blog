a438707584ce07a8820c51c216d436c30f320f22
alexander <alexander-1313>
2021-01-31 01:08:51 +0300

    update utils v2.0

--- src/main/java/ru/leverx/blog/util/RequestUtil.java
+++ src/main/java/ru/leverx/blog/util/RequestUtil.java
@@ -1,18 +1,15 @@
 package ru.leverx.blog.util;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
 import ru.leverx.blog.configuration.jwt.JwtProvider;
+import ru.leverx.blog.entity.Article;
+import ru.leverx.blog.entity.User;
+import ru.leverx.blog.service.ArticleService;
 import ru.leverx.blog.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
-
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
 
 import static org.springframework.util.StringUtils.hasText;
 import static ru.leverx.blog.configuration.jwt.JwtFilter.AUTHORIZATION;
@@ -20,12 +17,20 @@ import static ru.leverx.blog.configuration.jwt.JwtFilter.AUTHORIZATION;
 @Service
 public class RequestUtil {
 
-    @Autowired
     private JwtProvider jwtProvider;
-    @Autowired
     private UserService userService;
+    private Jedis jedis;
+    private ArticleService articleService;
 
-    public static String getTokenFromRequest(HttpServletRequest request) {
+    @Autowired
+    public RequestUtil(JwtProvider jwtProvider, UserService userService, Jedis jedis, ArticleService articleService) {
+        this.jwtProvider = jwtProvider;
+        this.userService = userService;
+        this.jedis = jedis;
+        this.articleService = articleService;
+    }
+
+    public String getTokenFromRequest(HttpServletRequest request) {
         String bearer = request.getHeader(AUTHORIZATION);
         if (hasText(bearer) && bearer.startsWith("Bearer ")) {
             return bearer.substring(7);
@@ -34,9 +39,37 @@ public class RequestUtil {
     }
 
     public boolean isConfirmUser(HttpServletRequest request, String id){
-        String requestToken = RequestUtil.getTokenFromRequest(request);
+        String requestToken = getTokenFromRequest(request);
         String generatedTokenFromRequestId = jwtProvider.generateToken(userService.findById(Integer.parseInt(id)).getEmail());
         return requestToken.equals(generatedTokenFromRequestId);
+    }
+
+    public boolean hasAccess(HttpServletRequest request){
+        String token = getTokenFromRequest(request);
+        String email = jwtProvider.getLoginFromToken(token);
+        User user = userService.findByEmail(email);
+        return user != null;
+    }
+
+    public boolean hasArticle(HttpServletRequest request, Integer articleId){
+        String token = getTokenFromRequest(request);
+        String email = jwtProvider.getLoginFromToken(token);
+        User user = userService.findByEmail(email);
+        Article article = articleService.findById(articleId);
+        User userDb = userService.findByArticle(article);
+
+        return userDb == user;
+    }
+
+    public Integer getUserIdByRequest(HttpServletRequest request){
+        String token = getTokenFromRequest(request);
+        String email = jwtProvider.getLoginFromToken(token);
+        User user = userService.findByEmail(email);
+        if(user != null){
+            return user.getId();
+        }
+
+        return -1;
     }
 
     public Integer strToInt(String str){
