b37b4b14ba19cc0630d3ac9ec26b7e5184c08a7d
alexander <alexander-1313>
2021-01-30 04:34:20 +0300

    update post mapping

--- src/main/java/ru/leverx/blog/controller/ArticleController.java
+++ src/main/java/ru/leverx/blog/controller/ArticleController.java
@@ -5,16 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.leverx.blog.entity.Article;
 import ru.leverx.blog.entity.Status;
+import ru.leverx.blog.entity.Tag;
 import ru.leverx.blog.entity.User;
 import ru.leverx.blog.service.ArticleService;
+import ru.leverx.blog.service.TagService;
 import ru.leverx.blog.service.UserService;
 import ru.leverx.blog.util.RequestUtil;
 import ru.leverx.blog.util.View;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/users/{userId}/articles")
@@ -23,12 +23,14 @@ public class ArticleController {
     private ArticleService articleService;
     private UserService userService;
     private RequestUtil requestUtil;
+    private TagService tagService;
 
     @Autowired
-    public ArticleController(ArticleService articleService, UserService userService, RequestUtil requestUtil) {
+    public ArticleController(ArticleService articleService, UserService userService, RequestUtil requestUtil, TagService tagService) {
         this.articleService = articleService;
         this.userService = userService;
         this.requestUtil = requestUtil;
+        this.tagService = tagService;
     }
 
     @JsonView(View.UI.class)
@@ -37,12 +39,26 @@ public class ArticleController {
                            @RequestParam("title") String title,
                            @RequestParam("text") String text,
                            @RequestParam("status") String status,
+                           @RequestParam("tags") Set<String> tags,
                            HttpServletRequest request){
         Article article = null;
+        List<Tag> tagSet = new ArrayList<>();
+        Tag dbTag;
+
+        for(String tagName: tags){
+            dbTag = tagService.findByName(tagName);
+
+            if(dbTag == null) {
+                Tag tag = new Tag(tagName);
+                tagSet.add(tag);
+            }else{
+                tagSet.add(dbTag);
+            }
+        }
 
         if(requestUtil.isConfirmUser(request, userId)) {
             User user = userService.findById(Integer.parseInt(userId));
-            article = new Article(title, text, Status.valueOf(status), new Date(), new Date(), user);
+            article = new Article(title, text, Status.valueOf(status), new Date(), new Date(), user, tagSet);
             articleService.save(article);
         }
         return article;
@@ -51,7 +67,7 @@ public class ArticleController {
     @JsonView(View.UI.class)
     @GetMapping
     public List<Article> getAllPublicPosts(@PathVariable String userId,
-                                           @RequestParam Map<String, String> allRequestParam) { //TODO
+                                           @RequestParam Map<String, String> allRequestParam) {
 
         Integer skip = Integer.parseInt(allRequestParam.get("skip"));
         Integer limit = Integer.parseInt(allRequestParam.get("limit"));
