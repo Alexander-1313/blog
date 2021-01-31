c00237f54aea42775abb7adad4391d04761556d6
alexander <alexander-1313>
2021-01-31 19:39:58 +0300

    add constant class

---
+++ src/main/java/ru/leverx/blog/util/StringConstant.java
@@ -0,0 +1,26 @@
+package ru.leverx.blog.util;
+
+public class StringConstant {
+
+    public static final String JWT_SECREAT = "rybak";
+
+    public static final String EMAIL_PASSWORD = "a16171819";
+
+    public static final String EMAIL_NAME = "test.alexander.rybak@mail.ru";
+
+    public static final String JEDIS_HOST = "localhost";
+
+    public static final String SKIP = "skip";
+
+    public static final String LIMIT = "limit";
+
+    public static final String Q = "q";
+
+    public static final String AUTHOR_ID = "authorId";
+
+    public static final String ORDER = "order";
+
+    public static final String FIELD_NAME = "fieldName";
+
+
+}
